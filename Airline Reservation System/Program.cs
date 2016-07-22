3fff7904816c94d60084fddd73aed01a6e7cfed7
kferna34 <kferna34@my.centennialcollege.ca>
2016-07-21 21:58:35 -0400

    everything done

--- Airline Reservation System/Program.cs
+++ Airline Reservation System/Program.cs
@@ -5,50 +5,164 @@ using System.Text;
 using System.Threading.Tasks;
 
 /*
-*Assignment :           04 
-*Author:                Krytia Fernandez
-*Date Last Modified :   July 11, 2016
-*Description :          Array and List Practice
-*Version:               0.0.0.4 = fixed the display menu
-*/
+* Assignment :           04 
+* Author:                Krytia Fernandez
+* Date Last Modified :   July 18, 2016
+* Description :          Array and List Practice
+* Version:               0.0.0.10 == FINAL
+ */
 namespace Airline_Reservation_System
 {
     class Program
     {
+        private static bool[] seats;
+        public static int TotalAssignedFirstClass;
+        public static int TotalAssignedEconomyClass;
+
         static void Main(string[] args)
         {
             DisplayMenu();
         }
         static void DisplayMenu()
         {
-            int choice = 0;
-            while (choice != 3)
+            Random rand = new Random();// generates random
+            seats = new bool[11];
+            //To keep a separate list of seats taken        
+            List<int> seatsBooked = new List<int>();
+            int InputSelection = 0;//menu selection
+
+            do
             {
-                Console.WriteLine(" =========== Airline Reservation System ===========");
-                Console.WriteLine(" |                                                |");
-                Console.WriteLine(" |  1 -  First Class                              |");
-                Console.WriteLine(" |  2 -  Economy Class                            |");
-                Console.WriteLine(" |  3 -  Exit Reservation System                  |");
-                Console.WriteLine(" |                                                |");
-                Console.WriteLine(" ==================================================");
-                Console.Write(" Your Choice -> ");
-                choice = int.Parse(Console.ReadLine());
-                switch (choice)
+                bool SelectionMenu = true; // displays the menu
+                while (SelectionMenu)
+
                 {
-                    case 1:
-                        Console.WriteLine(" ==~~==~~ FIRST CLASS ~~==~~== ");
-                        break;
-                    case 2:
-                        Console.WriteLine("==~~==~~ ECONOMY CLASS ~~==~~== ");
-                        break;
-                    case 3:
-                        Console.WriteLine("==~~==~~ EXIT MENU ~~==~~== ");
-                        break;
-                    default:
-                        Console.WriteLine("PLEASE ENTER A RIGHT SELECTION !!");
-                        break;
-                }//switch
-            }//while
-        }//3rd end
-    }//2nd end
-}//end
+                    Console.WriteLine(" =========== Airline Reservation System ===========");
+                    Console.WriteLine(" |                                                |");
+                    Console.WriteLine(" |  1 -  First Class                              |");
+                    Console.WriteLine(" |  2 -  Economy Class                            |");
+                    Console.WriteLine(" |  3 -  Exit Reservation System                  |");
+                    Console.WriteLine(" |                                                |");
+                    Console.WriteLine(" ==================================================");
+                    Console.Write(" Your Choice ->  ", InputSelection);
+                    InputSelection = Int32.Parse(Console.ReadLine());
+                    Console.Clear();// clears the selection  to show a clear console again 
+                    switch (InputSelection)
+                    {
+                        case 1:
+
+                            if (TotalAssignedFirstClass == 5 && TotalAssignedEconomyClass < 5)
+                            {
+                                Console.WriteLine("Sorry, First class is full. Do you want to get a ticket for economy class? Y-N");
+                                string choice = Console.ReadLine();
+                                if (choice.Equals("Y", StringComparison.OrdinalIgnoreCase))//looks at the choice then compares it 
+                                {
+                                    BookEconomyClass();// refers to the bookEconomyClass method
+                                }
+                                else
+                                {
+                                    ChoiceNo();// refers to the Chocie no method
+                                }
+                            }
+                            else
+                            {
+                                BookFirstClass();
+                                Console.ReadKey();
+                            }
+                            if (TotalAssignedEconomyClass == 5 && TotalAssignedFirstClass == 5)
+                            {
+                                Console.ReadKey();
+                                Console.WriteLine("Sorry! The plane is full. The next plane will be leaving in 3 hours");
+
+                            }
+                            break;
+
+                        case 2:
+                            if (TotalAssignedEconomyClass == 5 && TotalAssignedFirstClass < 5)
+                            {
+                                Console.WriteLine("Sorry, Economy Class is full. Do you want to get a ticket for first class? Y-N");
+                                string choice = Console.ReadLine();
+                                if (choice.Equals("Y", StringComparison.OrdinalIgnoreCase))
+                                {
+                                    BookFirstClass();
+                                }
+                                else
+                                {
+                                    ChoiceNo();
+                                }
+                            }
+                            else
+                            {
+
+                                BookEconomyClass();
+                                Console.ReadKey();
+                            }
+                            if (TotalAssignedEconomyClass == 5 && TotalAssignedFirstClass == 5)
+                            {
+                                Console.ReadKey();
+                                Console.WriteLine("Sorry! The plane is full. The next plane will be leaving in 3 hours");
+                                break;
+                            }
+                            break;
+
+                        case 3:
+
+                            Console.WriteLine("==~~==~~ SEE YOU  AGAIN SOON!! ~~==~~== ");
+                            SelectionMenu = false;
+                            break;
+                        default:
+                            Console.WriteLine(" INVALID SELECTION !!");
+                            break;
+                    }
+                }
+            } while (InputSelection != 3);
+        }
+
+        public static void BookFirstClass()
+        {
+
+            bool SeatRepeated = false;
+            Random rand = new Random();
+            int index = 0;
+
+            //keep generating the seat number until a FREE seat is assigned
+            while (!SeatRepeated)
+            {
+                SeatRepeated = true;
+                index = rand.Next(1, 6);
+                if (seats[index] == true)
+                    SeatRepeated = false;
+            }
+            seats[index] = true;
+            TotalAssignedFirstClass++;
+            Console.WriteLine("Assigned seat {0:N0}", index);
+            Console.WriteLine("Please press ENTER to show the selection");
+        }
+
+        public static void BookEconomyClass()
+        {
+
+            bool seatRepeat = false;
+            Random rand = new Random();
+            int index = 0;
+
+            //keep generating the seat number until a FREE seat is assigned
+            while (!seatRepeat)
+            {
+                seatRepeat = true;// avoids repeating number or seats in this matter
+                index = rand.Next(6, 11);
+                if (seats[index] == true)
+                    seatRepeat = false;
+            }
+            seats[index] = true;
+            TotalAssignedEconomyClass++;
+            Console.WriteLine("Assigned seat {0:N0}", index);
+            Console.WriteLine("Please press ENTER to show the selection");
+        }
+        public static void ChoiceNo()
+        {
+            Console.WriteLine(" The Next plane leaves in 3 hours");
+
+        }
+    }
+}
--- Assignment04/Program.cs
+++ Assignment04/Program.cs
@@ -10,17 +10,39 @@ using System.Threading.Tasks;
 *Author:                Krytia Fernandez
 *Date Last Modified :   July 09, 2016
 *Description :          Array and List Practice
-*Version:               0.0.0.3 = AIRLINE RESERVATION SYSTEM SWITCH    
+*Version:               0.0.0.10 = all done
 */
 namespace Assignment04
 {
-    class Program
+        class Program
     {
         static void Main(string[] args)
         {
+            Random rand = new Random(); // random number object
+            List<int> diceRolling = new List<int>(); // New List of Type int
+            int[] list = new int[13];
+            int MaximumTimes = 36000;
+
+            // Build the list to store random numbers of dices
+            for (int k = 0; k < MaximumTimes; k++)
+            {
+                int DieOne = rand.Next(1, 7);
+                int DieTwo = rand.Next(1, 7);
+                int sum = DieOne + DieTwo;
+                list[sum]++;
+                diceRolling.Add(sum);
+            }
+            Console.WriteLine("||::::::::::::::::::::::::||");
+            Console.WriteLine("|| Rolls  |Number of Rolls||");
+            Console.WriteLine("||+++****+++****+++****+++||");
+            for (int k = 2; k < 13; k++)
+            {
+                Console.WriteLine("||{0,4}    |{1,10}     ||", k, list[k]);
+                Console.WriteLine("||::::::::::::::::::::::::||");
+
+            }
 
         }
-
     }
 }
 
